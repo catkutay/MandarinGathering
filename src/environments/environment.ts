3c27bc951327316f9bb3e48f4b1321a06a88dfa8
Cat Kutay <cat.kutay@cdu.edu.au>
2022-04-10 13:34:36 +0930

    fixed credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@ export var firebaseConfig = {
   production: false,
   useEmulators: true,
   apiKey: 'AIzaSyDhuDC4kWZvwrpr-3ueD5qRCvlufpy6z2I',
-  authDomain: 'Mandaringathering-d91d4.firebaseapp.com',
+  authDomain: 'mandaringathering-d91d4.firebaseapp.com',
   databaseURL:
-    'https://Mandaringathering-d91d4-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'Mandaringathering-d91d4',
-  storageBucket: 'Mandaringathering-d91d4.appspot.com',
+    'https://mandaringathering-d91d4-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'mandaringathering-d91d4',
+  storageBucket: 'mandaringathering-d91d4.appspot.com',
   messagingSenderId: '477043088588',
   appId: '1:477043088588:web:27aa798583cbbcf30cb669',
 };
